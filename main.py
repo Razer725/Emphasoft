d69392af59f6dce89452a596f694f24fde50307e
Ivan Krasilnikov <krasivan93@gmail.com>
2021-01-17 17:08:28 +0200

    Add token auth

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import uvicorn
 
-from typing import Optional, Dict
+from datetime import timedelta
 from fastapi.encoders import jsonable_encoder
-from fastapi import APIRouter, Body, FastAPI
+from fastapi import APIRouter, FastAPI, Depends, HTTPException, status
 from fastapi.responses import Response
-from models import UserIn, UserOut, UserPatch, create_user, get_users, user_helper, get_user, update_user_data, delete_user_data
+from fastapi.security import OAuth2PasswordRequestForm
+
+from models import UserIn, UserOut, Token, UserPatch, create_user, get_users, update_user_data, \
+    delete_user_data, get_user_data, authenticate_user, create_access_token, get_current_user
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 app = FastAPI()
 router = APIRouter()
@@ -22,6 +27,33 @@ def error_response_model(error, code, message):
     return {"error": error, "code": code, "message": message}
 
 
+@app.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@app.get("/users/")
+def users_list():
+    users = get_users()
+    return users
+
+
+@app.get("/users/me/", response_model=UserOut)
+async def read_users_me(current_user: UserOut = Depends(get_current_user)):
+    return current_user
+
+
 @app.post("/create", response_model=UserOut)
 def create_user_data(user: UserIn):
     print(user, 'before')
@@ -31,15 +63,9 @@ def create_user_data(user: UserIn):
     return Response(content=new_user, media_type="application/json")
 
 
-@app.get("/users/")
-def users_list():
-    users = get_users()
-    return users
-
-
 @app.get("/users/{id}", response_description='User data retrieved')
 def user_read(id: str):
-    user = get_user(id)
+    user = get_user_data(id)
     return user
 
 
--- models.py
+++ models.py
@@ -1,46 +1,87 @@
-from typing import Optional, Dict
-from pydantic import BaseModel, Field
+from typing import Optional
+from pydantic import BaseModel
 from pymongo import MongoClient
-import datetime
-from bson import ObjectId, json_util, SON
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from bson import ObjectId, json_util
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
 
+SECRET_KEY = 'a6a5d44dfd87a5dc2d4576ed6692ff652ec6ae69b670ef40617f60e55fd009bf'
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 client = MongoClient('localhost', 27017)
 db = client['Emphasoft-database']
 collection = db['Emphasoft-collection']
 
 
-class UserIn(BaseModel):
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
+
+
+class UserBase(BaseModel):
     username: str
-    password: str
-    first_name: str
-    last_name: str
-    is_active: bool = True
-    last_login: datetime.datetime
-    is_superuser: bool = False
-
-
-class UserPatch(BaseModel):
-    username: Optional[str]
-    password: Optional[str]
     first_name: Optional[str]
     last_name: Optional[str]
     is_active: Optional[bool]
-    last_login: Optional[datetime.datetime]
+
+
+class UserIn(UserBase):
+    password: str
+    last_login: datetime
+
+
+class UserPatch(UserBase):
+    password: Optional[str]
+    last_login: Optional[datetime]
     is_superuser: Optional[bool]
 
 
-class UserOut(BaseModel):
-    username: str
-    first_name: str
-    last_name: str
-    is_active: bool = True
-    last_login: datetime.datetime
+class UserOut(UserBase):
+    is_active: bool
+    last_login: datetime
+    is_superuser: bool
+
+
+class UserInDB(UserBase):
     is_superuser: bool = False
-
-
-class UserInDB(UserIn):
+    last_login: Optional[datetime]
     hashed_password: str
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def get_user(username: str):
+    user = collection.find_one({'username': username})
+    print(user, 'user')
+    if user:
+        return UserInDB(**user)
+
+
+def authenticate_user(username: str, password: str):
+    user = get_user(username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
 
 
 def user_helper(user) -> dict:
@@ -55,7 +96,40 @@ def user_helper(user) -> dict:
     }
 
 
-def create_user(data):
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+def create_user(data) -> UserInDB:
+    data['hashed_password'] = get_password_hash(data['password'])
+    data.pop('password')
     user = collection.insert_one(data)
     new_user = collection.find_one({'_id': user.inserted_id})
     return json_util.dumps(new_user)
@@ -68,27 +142,30 @@ def get_users():
     return users
 
 
-def get_user(id: str) -> dict:
-    user = collection.find_one({"_id": ObjectId(id)})
+def get_user_data(_id: str) -> dict:
+    user = collection.find_one({"_id": ObjectId(_id)})
     if user:
         return user_helper(user)
 
 
-def update_user_data(id: str, data):
-    if len(data) < 1:   # Check body is not empty
+def update_user_data(_id: str, data):
+    if len(data) < 1:  # Check body is not empty
         return False
-    user = collection.find_one({"_id": ObjectId(id)})
+    if 'password' in data:
+        data['hashed_password'] = get_password_hash(data['password'])
+        data.pop('password')
+    user = collection.find_one({"_id": ObjectId(_id)})
     if user:
         updated_user = collection.update_one(
-                    {"_id": ObjectId(id)}, {"$set": data}
-                )
+            {"_id": ObjectId(_id)}, {"$set": data}
+        )
         if updated_user:
             return True
         return False
 
 
-def delete_user_data(id: str):
-    user = collection.find_one({"_id": ObjectId(id)})
+def delete_user_data(_id: str):
+    user = collection.find_one({"_id": ObjectId(_id)})
     if user:
-        collection.delete_one({"_id": ObjectId(id)})
+        collection.delete_one({"_id": ObjectId(_id)})
         return True
